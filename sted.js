7aee09b6fb0f44ed5a2ad9e6f4dc37c68ab7f6d8
William Hueston <whueston@my.wctc.edu>
2020-12-04 17:44:16 -0600

    Initial commit

---
+++ sted.js
@@ -0,0 +1,13 @@
+$(document).ready(
+    function () {
+        //This first bit before my event listener is so that the rules popup will follow your mouse as your move it.
+        var chryslerhero = document.getElementById('mcDisclaimer');
+
+        window.onmousemove = function (e) {
+            var x = e.clientX,
+                y = e.clientY;
+            chryslerhero.style.top = (y + 20) + 'px';
+            chryslerhero.style.left = (x + 20) + 'px';
+        };
+    }
+)
